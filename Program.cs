21b9c3d6e345f32c011b1bb1ec489bdc6e36424e
SheikhAmin785 <shakil785ks@gmail.com>
2023-12-04 21:37:20 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LinearSearchSorted
+{
+    class Program
+    {
+        static int LinearSearch(int[] arr, int n, int value)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (value == arr[i])
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int n = 4;
+            int result = LinearSearch(arr, arr.Length, n);
+            Console.WriteLine("The item position: "+result);
+
+        }
+    }
+}
